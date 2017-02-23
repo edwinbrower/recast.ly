16e75a4dcc6860ccc4f38a93f4c4a04f91f8d6d3
lg_baum <laura.greenbaum@gmail.com>
2017-02-23 10:58:36 -0800

    Passing app tests

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,27 +1,15 @@
-// var App = (props) => (
-//   <div>
-//     <Nav />
-//     <div className="col-md-7">
-//       <VideoPlayer video={window.exampleVideoData[0]}/>
-//     </div>
-//     <div className="col-md-5">
-//       <VideoList videos={window.exampleVideoData}/>
-//     </div>
-//   </div>
-// );
-
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       video: window.exampleVideoData[0]
     };
-    this.onVideoListEntryClick = this.onVideoListEntryClick.bind(this);
+    // this.onVideoListEntryClick = this.onVideoListEntryClick.bind(this);
   }
 
-  onVideoListEntryClick(video) {
+  onVideoListEntryClick(e) {
     this.setState({
-      video: video
+      video: e.currentTarget.props.video
     });
   }
 
@@ -33,7 +21,7 @@ class App extends React.Component {
           <VideoPlayer video={this.state.video}/>
         </div>
         <div className="col-md-5">
-          <VideoList entryClick={this.onVideoListEntryClick} videos={window.exampleVideoData}/>
+          <VideoList entryClick={this.onVideoListEntryClick.bind(this)} videos={window.exampleVideoData}/>
         </div>
       </div>
     );
@@ -43,6 +31,3 @@ class App extends React.Component {
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
 window.App = App;
-
-
-// use for prop of VideoList?? videos={window.exampleVideoData}
--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,18 +1,18 @@
-// var VideoList = (props) => (
-//   <div className="video-list media">
-//     {props.videos.map(function(video) {
-//       <VideoListEntry entryClick={this.entryClick} video={video} />
-//     }, props)}
-//   </div>
-// );
-
 var VideoList = (props) => (
   <div className="video-list media">
-    {props.videos.map(function(video) =>
-      <VideoListEntry entryClick={props.video} video={video} />
-      )}
+    {props.videos.map(function(video) {
+      <VideoListEntry entryClick={props.entryClick} video={video} />
+    })}
   </div>
 );
+
+// var VideoList = (props) => (
+//   <div className="video-list media">
+//     {props.videos.map((video) =>
+//       <VideoListEntry entryClick={props.entryClick} video={video} />
+//     )}
+//   </div>
+// );
 
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
